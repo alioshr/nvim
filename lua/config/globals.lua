a68a244fc9a6cb0254eca7422565e73be81a189d
Aliosh Pimenta <alioshr@protonmail.com>
2025-07-22 00:41:23 +0200

    setup number file navigation

--- lua/config/globals.lua
+++ lua/config/globals.lua
@@ -1,3 +1,8 @@
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 vim.g.mapleader = " " -- easy to reach leader key
+
+-- Show numbers on the lines
+vim.opt.number = true
+vim.api.nvim_create_autocmd("InsertEnter", { command = [[set norelativenumber]] })
+vim.api.nvim_create_autocmd("InsertLeave", { command = [[set relativenumber]] })

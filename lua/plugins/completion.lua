aa3763b3deb9ee252becef40c8cd411cbdcc6a54
Aliosh Pimenta <alioshr@protonmail.com>
2025-07-23 20:55:39 +0200

    refator: change copilot config to properly suggest autocomplete

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -20,15 +20,7 @@ return {
 		completion = { documentation = { auto_show = true } },
 
 		sources = {
-			default = { 'lsp', 'path', 'snippets', 'buffer', 'copilot' },
-			providers = {
-				copilot = {
-					name = "copilot",
-					module = "blink-cmp-copilot",
-					score_offset = 100,
-					async = true,
-				},
-			},
+			default = { 'lsp', 'path', 'snippets', 'buffer' },
 		},
 
 		fuzzy = { implementation = "prefer_rust_with_warning" }

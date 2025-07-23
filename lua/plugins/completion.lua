48238e848d8b4bf1a6afbc9ec2756095a67cfe98
Aliosh Pimenta <alioshr@protonmail.com>
2025-07-23 18:46:19 +0200

    add support for copilot

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,29 +1,37 @@
 return {
-    'saghen/blink.cmp',
-    dependencies = { 'rafamadriz/friendly-snippets' },
+	'saghen/blink.cmp',
+	dependencies = { 'rafamadriz/friendly-snippets', 'giuxtaposition/blink-cmp-copilot' },
 
-    version = '1.*',
+	version = '1.*',
 
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-        keymap = {
-            preset = 'default',
-            ['<CR>'] = { 'accept', 'fallback' },
-            ['<C><leader>'] = { 'show' }
-        },
+	---@module 'blink.cmp'
+	---@type blink.cmp.Config
+	opts = {
+		keymap = {
+			preset = 'default',
+			['<CR>'] = { 'accept', 'fallback' },
+			['<C><leader>'] = { 'show' }
+		},
 
-        appearance = {
-            nerd_font_variant = 'mono'
-        },
+		appearance = {
+			nerd_font_variant = 'mono'
+		},
 
-        completion = { documentation = { auto_show = true } },
+		completion = { documentation = { auto_show = true } },
 
-        sources = {
-            default = { 'lsp', 'path', 'snippets', 'buffer' },
-        },
+		sources = {
+			default = { 'lsp', 'path', 'snippets', 'buffer', 'copilot' },
+			providers = {
+				copilot = {
+					name = "copilot",
+					module = "blink-cmp-copilot",
+					score_offset = 100,
+					async = true,
+				},
+			},
+		},
 
-        fuzzy = { implementation = "prefer_rust_with_warning" }
-    },
-    opts_extend = { "sources.default" }
+		fuzzy = { implementation = "prefer_rust_with_warning" }
+	},
+	opts_extend = { "sources.default" }
 }

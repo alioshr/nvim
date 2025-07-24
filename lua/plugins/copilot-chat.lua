9626a20300979fcaf2d1140129b12ae60b487ef2
Aliosh Pimenta <alioshr@protonmail.com>
2025-07-25 01:11:23 +0200

    refactor: improve copilot keymaps description

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -10,15 +10,15 @@ return {
 			-- See Configuration section for options
 		},
 		keys = {
-			{ "<Leader>zc", ":CopilotChat<CR>",         mode = "n", desc = "Chat with Copilot" },
-			{ "<Leader>ze", ":CopilotChatExplain<CR>",  mode = "v", desc = "Explain Code" },
-			{ "<Leader>zr", ":CopilotChatReview<CR>",   mode = "v", desc = "Review Code" },
-			{ "<Leader>zf", ":CopilotChatFix<CR>",      mode = "v", desc = "Fix Code Issues" },
-			{ "<Leader>zo", ":CopilotChatOptimize<CR>", mode = "v", desc = "Optimize Code" },
-			{ "<Leader>zd", ":CopilotChatDocs<CR>",     mode = "v", desc = "Generate Docs" },
-			{ "<Leader>zt", ":CopilotChatTests<CR>",    mode = "v", desc = "Generate Tests" },
-			{ "<Leader>zm", ":CopilotChatCommit<CR>",   mode = "n", desc = "Generate Commit Message" },
-			{ "<Leader>zs", ":CopilotChatCommit<CR>",   mode = "v", desc = "Generate Commit for Selection" },
+			{ "<Leader>zc", ":CopilotChat<CR>",         mode = "n", desc = "Copilot - Chat with Copilot" },
+			{ "<Leader>ze", ":CopilotChatExplain<CR>",  mode = "v", desc = "Copilot - Explain Code" },
+			{ "<Leader>zr", ":CopilotChatReview<CR>",   mode = "v", desc = "Copilot - Review Code" },
+			{ "<Leader>zf", ":CopilotChatFix<CR>",      mode = "v", desc = "Copilot - fix Code Issues" },
+			{ "<Leader>zo", ":CopilotChatOptimize<CR>", mode = "v", desc = "Copilot - Optimize Code" },
+			{ "<Leader>zd", ":CopilotChatDocs<CR>",     mode = "v", desc = "Copilot - Generate Docs" },
+			{ "<Leader>zt", ":CopilotChatTests<CR>",    mode = "v", desc = "Copilot - Generate Tests" },
+			{ "<Leader>zm", ":CopilotChatCommit<CR>",   mode = "n", desc = "Copilot - Generate Commit Message" },
+			{ "<Leader>zs", ":CopilotChatCommit<CR>",   mode = "v", desc = "Copilot - Generate Commit for Selection" },
 		}
 
 	},

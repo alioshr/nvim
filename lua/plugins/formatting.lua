d5533b769915aed2c8eb2b1235e799a24cd61431
Aliosh Pimenta <alioshr@protonmail.com>
2025-07-26 00:41:03 +0200

    feat: make prettier work as fallback when no eslint uses prettier if a prettier config file is present

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -9,17 +9,24 @@ return {
 					javascript = { "prettier" },
 					javascriptreact = { "prettier" },
 					typescript = { "prettier" },
+					typescriptreact = { "prettier" }, -- Add this if you use TSX
 					json = { "prettier" },
 					graphql = { "prettier" },
 				},
 				formatters = {
 					prettier = {
 						require_cwd = true,
+						-- Ensure it finds your project's prettier config
+						condition = function(ctx)
+							return vim.fs.find(
+								{ ".prettierrc", ".prettierrc.json", ".prettierrc.js", "prettier.config.js" },
+								{ path = ctx.filename, upward = true }
+							)[1]
+						end,
 					},
 				},
 				format_on_save = {
 					timeout_ms = 500,
-					lsp_format = "fallback",
 				},
 			})
 		end,
@@ -29,6 +36,5 @@ return {
 		config = function()
 			require("nvim-ts-autotag").setup()
 		end,
-
 	}
 }

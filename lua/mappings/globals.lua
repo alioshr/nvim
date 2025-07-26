b2d764ad7d4c5890b526d9c087079a5ccd0ee47d
Aliosh Pimenta <alioshr@protonmail.com>
2025-07-26 23:56:38 +0200

    feat: set clear search keymap

--- lua/mappings/globals.lua
+++ lua/mappings/globals.lua
@@ -1,1 +1,7 @@
+-- Global key mappings for Neovim
+
+-- Navigate to the previous buffer
 vim.keymap.set("n", "-", vim.cmd.Ex)
+
+-- Clear search highlight
+vim.keymap.set("n", "<leader>us", ":noh<CR>", { desc = "Clear search highlight" })

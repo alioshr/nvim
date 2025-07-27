024cf2aac10e22978e142b757b1bd677b60eeeb6
Aliosh Pimenta <alioshr@protonmail.com>
2025-07-27 13:03:02 +0200

    feat: style diagnostics underline color

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -15,7 +15,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
 vim.diagnostic.config({
   virtual_text = false,
-  signs = true,
+  signs = false,
   underline = true,
   update_in_insert = true,
   float = {
@@ -25,3 +25,9 @@ vim.diagnostic.config({
     prefix = "",
   },
 })
+
+-- Configure diagnostics underline styles for error, warning and info
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { undercurl = true, underdouble = true, sp = "#E06C75" })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineWarn", { undercurl = true, underdouble = true, sp = "#E5C07B" })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineInfo", { undercurl = true, underdouble = true, sp = "#61AFEF" })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineHint", { undercurl = true, underdouble = true, sp = "#C678DD" })

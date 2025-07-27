7e1d87518d7caaa92e8789ea9255fa4b10912cc3
Aliosh Pimenta <alioshr@protonmail.com>
2025-07-27 11:44:30 +0200

    feat: add mini.identscope & mini.diff
    mini diff required diffstatic and delta pager to work nicely

--- lua/plugins/mini-nvim.lua
+++ lua/plugins/mini-nvim.lua
@@ -7,5 +7,23 @@ return {
     require("mini.operators").setup()
     require("mini.pairs").setup()
     require("mini.bracketed").setup()
+    require("mini.indentscope").setup()
+    require("mini.diff").setup({
+      view = {
+        style = "sign",
+      },
+    })
   end,
+  keys = {
+    {
+      "<leader>gv",
+      function()
+        require("mini.diff").toggle_overlay()
+      end,
+      desc = "Toggle Comment",
+    },
+  },
 }
+
+-- baa
+-- -aba

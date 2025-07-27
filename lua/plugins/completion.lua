335fb9d4ae34b9f69a7cfaff2f01a9afb21e3883
Aliosh Pimenta <alioshr@protonmail.com>
2025-07-27 02:04:39 +0200

    feat: add copilot lsp
    copilot lsp has multi line suggestions

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,6 +1,6 @@
 return {
   "saghen/blink.cmp",
-  dependencies = { "rafamadriz/friendly-snippets", "giuxtaposition/blink-cmp-copilot" },
+  dependencies = { "rafamadriz/friendly-snippets" },
 
   version = "1.*",
 
@@ -8,22 +8,39 @@ return {
   ---@type blink.cmp.Config
   opts = {
     keymap = {
-      preset = "default",
+      preset = "super-tab",
       ["<CR>"] = { "accept", "fallback" },
       ["<C><leader>"] = { "show" },
+      ["<Tab>"] = {
+        function(cmp)
+          if vim.b[vim.api.nvim_get_current_buf()].nes_state then
+            cmp.hide()
+            return (
+              require("copilot-lsp.nes").apply_pending_nes()
+              and require("copilot-lsp.nes").walk_cursor_end_edit()
+            )
+          end
+          if cmp.snippet_active() then
+            return cmp.accept()
+          else
+            return cmp.select_and_accept()
+          end
+        end,
+        "snippet_forward",
+        "fallback",
+      },
     },
-
-    appearance = {
-      nerd_font_variant = "mono",
-    },
-
-    completion = { documentation = { auto_show = true } },
-
-    sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
-    },
-
-    fuzzy = { implementation = "prefer_rust_with_warning" },
   },
-  opts_extend = { "sources.default" },
+
+  appearance = {
+    nerd_font_variant = "mono",
+  },
+
+  completion = { documentation = { auto_show = true } },
+
+  sources = {
+    default = { "lsp", "path", "snippets", "buffer", "copilot" },
+  },
+
+  fuzzy = { implementation = "prefer_rust_with_warning" },
 }

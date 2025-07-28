b8114279fc2c070b6ce1a7ee82afd7193d8f9cf0
Aliosh Pimenta <alioshr@protonmail.com>
2025-07-28 22:32:02 +0200

    feat: add dd for escape & comment lps commands taken by snaks

--- lua/mappings/globals.lua
+++ lua/mappings/globals.lua
@@ -5,6 +5,10 @@ vim.keymap.set("n", "-", vim.cmd.Ex, { desc = "Open file explorer" })
 
 -- Escape insert mode
 vim.keymap.set("i", "jj", "<Esc>", { desc = "Escape insert mode with jj" })
+vim.keymap.set("i", "dd", "<Esc>", { desc = "Escape insert mode with dd" })
 
 -- Clear search highlight
 vim.keymap.set("n", "<leader>us", ":noh<CR>", { desc = "Clear search highlight" })
+
+-- Copy the path of the current buffer
+vim.keymap.set("n", "<leader>cp", ":let @+=expand('%:p')<CR>", { desc = "Copy the path of the current buffer" })

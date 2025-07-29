63cf8ca7a421794361bdc3abfe69b225cfcc0171
Aliosh Pimenta <alioshr@protonmail.com>
2025-07-29 14:28:07 +0200

    feat: attempt to improve lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -28,6 +28,30 @@ return {
             client.server_capabilities.documentFormattingProvider = false
             require("workspace-diagnostics").populate_workspace_diagnostics(client, 0)
           end,
+          settings = {
+            typescript = {
+              inlayHints = {
+                includeInlayParameterNameHints = "literal",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = false,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+              },
+            },
+            javascript = {
+              inlayHints = {
+                includeInlayParameterNameHints = "all",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+              },
+            },
+          },
         },
         eslint = {},
         tailwindcss = {},

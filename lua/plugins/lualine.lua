c2c6fa59958db28cfcd4bf91a5d91e635b731140
allpepper <alioshr@protonmail.com>
2025-07-31 22:26:16 +0200

    feat: add dir name on lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,12 +3,23 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
+      local function get_dir_name()
+        local cwd = vim.fn.getcwd()
+        return vim.fn.fnamemodify(cwd, ":t")
+      end
+
       require("lualine").setup({
         options = {
           theme = "catppuccin",
         },
         sections = {
-          lualine_a = {},
+          lualine_a = {
+            {
+              get_dir_name,
+              icon = "",
+              color = { gui = "bold" },
+            },
+          },
           lualine_c = { {
             "filename",
             path = 1,

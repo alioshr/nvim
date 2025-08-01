d374e6fc304b3cdb6dd491e1dfaac5022aa12ae5
allpepper <alioshr@protonmail.com>
2025-08-02 01:37:29 +0200

    fix: treesitter config typo

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     lazy = false,
-    build = ":TSUpdae",
+    build = ":TSUpdate",
     event = { "BufReadPre", "BufNewFile" },
     dependencies = {
       "nvim-treesitter/nvim-treesitter-textobjects",
@@ -10,7 +10,10 @@ return {
     config = function()
       require("nvim-treesitter.configs").setup({
         -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+        ensure_installed = { 
+          "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline",
+          "javascript", "typescript", "tsx", "json", "css", "html"
+        },
 
         -- Install parsers synchronously (only applied to `ensure_installed`)
         sync_install = false,

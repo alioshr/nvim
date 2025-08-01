6fa7817f37ca341118a044837a78b2dee0deb0a9
allpepper <alioshr@protonmail.com>
2025-08-01 03:41:37 +0200

    feat: improve neotest & add update snapshots for vite/jest command

--- lua/plugins/tests.lua
+++ lua/plugins/tests.lua
@@ -13,6 +13,20 @@ return {
     { "<leader>ti", "<cmd>Neotest output<cr>", desc = "Show test output" },
     { "<leader>ts", "<cmd>Neotest summary<cr>", desc = "Show test summary" },
     { "<leader>ta", '<cmd>lua require("neotest").run.run({ suite = true })<cr>', desc = "Run all tests" },
+    {
+      "<leader>tU",
+      function()
+        local current_file = vim.fn.expand("%:p")
+        if current_file:match("%.test%.") or current_file:match("%.spec%.") then
+          require("neotest").run.run({
+            vitestCommand = "npx vitest " .. current_file .. " -u",
+          })
+        else
+          print("Not a test file")
+        end
+      end,
+      desc = "Run current test file with update snapshots",
+    },
   },
   config = function()
     require("neotest").setup({
@@ -26,6 +40,9 @@ return {
             return vim.fn.getcwd()
           end,
         }),
+      },
+      discovery = {
+        enabled = false,
       },
     })
   end,

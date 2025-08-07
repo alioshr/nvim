e0f508cd784bb8d2d1c9fd6a144a0ddba428dcbf
allpepper <alioshr@protonmail.com>
2025-08-07 04:33:23 +0200

    feat: add window split key bindings

--- lua/mappings/globals.lua
+++ lua/mappings/globals.lua
@@ -26,3 +26,19 @@ vim.keymap.set("n", "<leader>Q", ":qa<CR>", { desc = "Quit all buffers" })
 
 --Source
 vim.keymap.set("n", "<leader>so", ":so %<CR>", { desc = "Source current file" })
+
+-- Split creation
+vim.keymap.set("n", "<localleader>ss", ":split<CR>", { desc = "Horizontal split" })
+vim.keymap.set("n", "<localleader>sv", ":vsplit<CR>", { desc = "Vertical split" })
+
+-- Split navigation
+vim.keymap.set("n", "<localleader>h", "<C-w>h", { desc = "Move to left split" })
+vim.keymap.set("n", "<localleader>j", "<C-w>j", { desc = "Move to bottom split" })
+vim.keymap.set("n", "<localleader>k", "<C-w>k", { desc = "Move to top split" })
+vim.keymap.set("n", "<localleader>l", "<C-w>l", { desc = "Move to right split" })
+
+-- Split resize
+vim.keymap.set("n", "<A-Left>", ":vertical resize -2<CR>", { desc = "Decrease split width" })
+vim.keymap.set("n", "<A-Down>", ":resize +2<CR>", { desc = "Increase split height" })
+vim.keymap.set("n", "<A-Up>", ":resize -2<CR>", { desc = "Decrease split height" })
+vim.keymap.set("n", "<A-Right>", ":vertical resize +2<CR>", { desc = "Increase split width" })

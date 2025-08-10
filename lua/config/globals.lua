a3d5448b112eba19d452544be19e45b3c5dae77f
allpepper <alioshr@protonmail.com>
2025-08-10 17:32:21 +0200

    doc: fix spacing

--- lua/config/globals.lua
+++ lua/config/globals.lua
@@ -1,9 +1,10 @@
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
-vim.g.mapleader = " " -- easy to reach leader key
+vim.g.mapleader = " "
 
 -- Show numbers on the lines
 vim.opt.number = true
+
 -- Show relative numbers
 vim.opt.relativenumber = true
 vim.opt.clipboard = "unnamedplus"

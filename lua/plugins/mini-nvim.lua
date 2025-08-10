66196d9eb505c3af44d2670f0debf21e5cd0e62e
allpepper <alioshr@protonmail.com>
2025-08-10 20:56:52 +0200

    feat: re-enable mini diff just for inline hunk visualization

--- lua/plugins/mini-nvim.lua
+++ lua/plugins/mini-nvim.lua
@@ -29,6 +29,15 @@ return {
         suffix_next = "n", -- Suffix to search with "next" method
       },
     })
+    -- Here we are just using mini.diff for inline hunk visualization and jumping between hunks.
+    -- The rest of this functionality is managed by GitSigns
+    require("mini.diff").setup({
+      view = {
+        style = "sign",
+        -- Here we force no signs to show, as we prefer Gitsigns signs
+        signs = { add = "", change = "", delete = "" },
+      },
+    })
     require("mini.operators").setup()
     require("mini.pairs").setup()
     require("mini.bracketed").setup()
@@ -71,63 +80,31 @@ return {
       -- idle time if user input is required.
       silent = false,
     })
-    require("mini.jump2d").setup({
-      -- Function producing jump spots (byte indexed) for a particular line.
-      -- For more information see |MiniJump2d.start|.
-      -- If `nil` (default) - use |MiniJump2d.default_spotter|
-      spotter = nil,
-
-      -- Characters used for labels of jump spots (in supplied order)
-      labels = "abcdefghijklmnopqrstuvwxyz",
-
-      -- Options for visual effects
-      view = {
-        -- Whether to dim lines with at least one jump spot
-        dim = false,
-
-        -- How many steps ahead to show. Set to big number to show all steps.
-        n_steps_ahead = 0,
-      },
-
-      -- Which lines are used for computing spots
-      allowed_lines = {
-        blank = true, -- Blank line (not sent to spotter even if `true`)
-        cursor_before = true, -- Lines before cursor line
-        cursor_at = true, -- Cursor line
-        cursor_after = true, -- Lines after cursor line
-        fold = true, -- Start of fold (not sent to spotter even if `true`)
-      },
-
-      -- Which windows from current tabpage are used for visible lines
-      allowed_windows = {
-        current = true,
-        not_current = true,
-      },
-
-      -- Functions to be executed at certain events
-      hooks = {
-        before_start = nil, -- Before jump start
-        after_jump = nil, -- After jump was actually done
-      },
-
-      -- Module mappings. Use `''` (empty string) to disable one.
-      mappings = {
-        start_jumping = "<CR>",
-      },
-
-      -- Whether to disable showing non-error feedback
-      -- This also affects (purely informational) helper messages shown after
-      -- idle time if user input is required.
-      silent = false,
-    })
+    require("mini.jump2d").setup()
   end,
+  -- Here we are just using mini.diff for inline hunk visualization and jumping between hunks.
+  -- The rest of this functionality is managed by GitSigns
   keys = {
     {
       "<leader>gv",
       function()
         require("mini.diff").toggle_overlay(0)
       end,
-      desc = "Toggle Git Diff Overlay",
+      desc = "MiniDiff: Toggle Git Diff Overlay",
+    },
+    {
+      "<leader>]+",
+      function()
+        require("mini.diff").goto_hunk("next")
+      end,
+      desc = "MiniDiff: Go to Next Git Diff Hunk",
+    },
+    {
+      "<leader>[è",
+      function()
+        require("mini.diff").goto_hunk("prev")
+      end,
+      desc = "MiniDiff: Go to Previous Git Diff Hunk",
     },
   },
 }

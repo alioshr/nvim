1ac5ec187d5790ca117571d5e2ba4830bcd2e600
allpepper <alioshr@protonmail.com>
2025-08-14 10:18:07 +0200

    feat(lsp): fallback to under curl with wezterm increased thickness

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -12,7 +12,7 @@ vim.diagnostic.config({
 })
 
 -- Configure diagnostics underline styles for error, warning and info
-vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { undercurl = true, underdouble = true, sp = "#E06C75" })
-vim.api.nvim_set_hl(0, "DiagnosticUnderlineWarn", { undercurl = true, underdouble = true, sp = "#E5C07B" })
-vim.api.nvim_set_hl(0, "DiagnosticUnderlineInfo", { undercurl = true, underdouble = true, sp = "#61AFEF" })
-vim.api.nvim_set_hl(0, "DiagnosticUnderlineHint", { undercurl = true, underdouble = true, sp = "#C678DD" })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { undercurl = true, sp = "#E06C75" })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineWarn", { undercurl = true, sp = "#E5C07B" })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineInfo", { undercurl = true, sp = "#61AFEF" })
+vim.api.nvim_set_hl(0, "DiagnosticUnderlineHint", { undercurl = true, sp = "#C678DD" })

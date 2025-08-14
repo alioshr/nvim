593adf5120ec280036f06ce21c36b14bcb222107
allpepper <alioshr@protonmail.com>
2025-08-14 03:52:22 +0200

    feat(theme): improve color for unused variables

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -3,9 +3,15 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    opts = {},
+    opts = {
+      style = "night",
+    },
     config = function()
       vim.cmd.colorscheme("tokyonight")
+      -- Override unused variable colors
+      vim.api.nvim_set_hl(0, "DiagnosticUnnecessary", { fg = "#8a92b3" })
+      vim.api.nvim_set_hl(0, "@variable.unused", { fg = "#8a92b3" })
+      vim.api.nvim_set_hl(0, "@parameter.unused", { fg = "#8a92b3" })
     end,
   },
 }

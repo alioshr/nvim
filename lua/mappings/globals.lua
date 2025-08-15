6da9c041397798c8d1a7b9fff98edb282d41853f
allpepper <alioshr@protonmail.com>
2025-08-15 05:13:46 +0200

    feat: add a mapping to run my floating tmux session

--- lua/mappings/globals.lua
+++ lua/mappings/globals.lua
@@ -53,3 +53,15 @@ vim.keymap.set("n", "<A-Right>", ":vertical resize +2<CR>", { desc = "Increase s
 
 -- Acess registers
 vim.keymap.set("n", "<localleader>r", ":registers<CR>", { desc = "Show registers" })
+
+-- Tmux floating terminal (default session)
+vim.keymap.set("n", "U", function()
+  local cwd = vim.fn.getcwd()
+  vim.fn.system(
+    string.format(
+      'tmux display-popup -E -b single -S fg=colour7 -h 95%% -w 90%% -x 9%% -y 42%% -d "%s" "tmux new-session -A -s popup-float -c \\"%s\\" \\; bind-key -n C-q detach-client \\; bind-key -n C-d kill-session"',
+      cwd,
+      cwd
+    )
+  )
+end, { desc = "Trigger tmux floating terminal (default session)" })

fd632e3032dc5fdc63233c09c40aa27f3674e9a2
allpepper <alioshr@protonmail.com>
2025-08-15 02:57:26 +0200

    fix(completion): remove deduplication hack
    Body:
    
    Deduplication hack would remove options with the same name part of other
    libraries

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -14,26 +14,6 @@ return {
     nerd_font_variant = "mono",
     highlight_ns = vim.api.nvim_create_namespace("blink_cmp"),
   },
-  -- Hack to remove duplicated items - https://github.com/Saghen/blink.cmp/issues/1222#event-18794466545
-  config = function(_, opts)
-    local original = require("blink.cmp.completion.list").show
-    ---@diagnostic disable-next-line: duplicate-set-field
-    require("blink.cmp.completion.list").show = function(ctx, items_by_source)
-      local seen = {}
-      local function filter(item)
-        if seen[item.label] then
-          return false
-        end
-        seen[item.label] = true
-        return true
-      end
-      for id, source in pairs(items_by_source) do
-        items_by_source[id] = source and vim.iter(source):filter(filter):totable()
-      end
-      return original(ctx, items_by_source)
-    end
-    require("blink.cmp").setup(opts)
-  end,
   opts = {
     keymap = {
       ["<cr>"] = { "accept", "fallback" },

393b42f629f93829f135127e9e8287711bd0cacd
allpepper <alioshr@protonmail.com>
2025-10-14 10:30:48 +0200

    chore(package-info): support breaking change highlight param

--- lua/plugins/package-info.lua
+++ lua/plugins/package-info.lua
@@ -2,10 +2,10 @@ return {
   "vuki656/package-info.nvim",
   dependencies = { "MunifTanjim/nui.nvim" },
   opts = {
-    colors = {
-      up_to_date = "#3C4048", -- Text color for up to date dependency virtual text
-      outdated = "#d19a66", -- Text color for outdated dependency virtual text
-      invalid = "#ee4b2b", -- Text color for invalid dependency virtual text
+    highlights = {
+      up_to_date = { fg = "#3C4048" }, -- Text color for up to date dependency virtual text
+      outdated = { fg = "#d19a66" }, -- Text color for outdated dependency virtual text
+      invalid = { fg = "#ee4b2b" }, -- Text color for invalid dependency virtual text
     },
     icons = {
       enable = true, -- Whether to display icons
@@ -29,8 +29,8 @@ return {
     require("package-info").setup(opts)
 
     -- manually register them
-    vim.cmd([[highlight PackageInfoUpToDateVersion guifg=]] .. opts.colors.up_to_date)
-    vim.cmd([[highlight PackageInfoOutdatedVersion guifg=]] .. opts.colors.outdated)
+    vim.cmd([[highlight PackageInfoUpToDateVersion guifg=]] .. opts.highlights.up_to_date.fg)
+    vim.cmd([[highlight PackageInfoOutdatedVersion guifg=]] .. opts.highlights.outdated.fg)
 
     vim.api.nvim_set_keymap(
       "n",

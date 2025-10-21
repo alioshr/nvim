998659c39f9f26a820a5e1618d58e2ebc32066eb
allpepper <alioshr@protonmail.com>
2025-10-21 11:51:17 +0200

    chore: update noice and remove hardcoded dependency commit

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -4,10 +4,8 @@ return {
   enabled = true,
   opts = {},
   dependencies = {
-    -- https://github.com/folke/noice.nvim/issues/1082
     {
       "MunifTanjim/nui.nvim",
-      commit = "8d3bce9764e627b62b07424e0df77f680d47ffdb",
     },
     {
       "rcarriga/nvim-notify",

717cbc501291bbab32325ea10d472f5d4e3c7e23
allpepper <alioshr@protonmail.com>
2025-12-03 09:52:07 +0100

    refactor: dap ui config

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -94,174 +94,7 @@ return {
       },
     })
 
-    for _, adapterType in ipairs({ "node", "chrome", "msedge" }) do
-      local pwaType = "pwa-" .. adapterType
-
-      dap.adapters[pwaType] = {
-        type = "server",
-        host = "localhost",
-        port = "${port}",
-        executable = {
-          command = "node",
-          args = {
-            vim.fn.stdpath("data") .. "/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js",
-            "${port}",
-          },
-        },
-      }
-
-      -- this allow us to handle launch.json configurations
-      -- which specify type as "node" or "chrome" or "msedge"
-      dap.adapters[adapterType] = function(cb, config)
-        local nativeAdapter = dap.adapters[pwaType]
-
-        config.type = pwaType
-
-        if type(nativeAdapter) == "function" then
-          nativeAdapter(cb, config)
-        else
-          cb(nativeAdapter)
-        end
-      end
-    end
-
-    local enter_launch_url = function()
-      local co = coroutine.running()
-      return coroutine.create(function()
-        vim.ui.input({ prompt = "Enter URL: ", default = "http://localhost:" }, function(url)
-          if url == nil or url == "" then
-            return
-          else
-            coroutine.resume(co, url)
-          end
-        end)
-      end)
-    end
-
-    for _, language in ipairs({ "typescript", "javascript", "typescriptreact", "javascriptreact", "vue" }) do
-      dap.configurations[language] = {
-        {
-          type = "pwa-node",
-          request = "launch",
-          name = "Launch file using Node.js (nvim-dap)",
-          program = "${file}",
-          cwd = "${workspaceFolder}",
-        },
-        {
-          type = "pwa-node",
-          request = "attach",
-          name = "Attach to process using Node.js (nvim-dap)",
-          processId = require("dap.utils").pick_process,
-          cwd = "${workspaceFolder}",
-        },
-        -- requires ts-node to be installed globally or locally
-        {
-          type = "pwa-node",
-          request = "launch",
-          name = "Launch file using Node.js with ts-node/register (nvim-dap)",
-          program = "${file}",
-          cwd = "${workspaceFolder}",
-          runtimeArgs = { "-r", "ts-node/register" },
-        },
-        {
-          type = "pwa-chrome",
-          request = "launch",
-          name = "Launch Chrome (nvim-dap)",
-          url = enter_launch_url,
-          webRoot = "${workspaceFolder}",
-          sourceMaps = true,
-        },
-        {
-          type = "pwa-msedge",
-          request = "launch",
-          name = "Launch Edge (nvim-dap)",
-          url = enter_launch_url,
-          webRoot = "${workspaceFolder}",
-          sourceMaps = true,
-        },
-      }
-    end
-
-    dap.adapters.coreclr = {
-      type = "executable",
-      command = "C:/Users/sfree/AppData/Local/nvim-data/mason/packages/netcoredbg/netcoredbg/netcoredbg.exe",
-      args = { "--interpreter=vscode" },
-    }
-
-    local dotnet_build_project = function()
-      local default_path = vim.fn.getcwd() .. "/"
-
-      if vim.g["dotnet_last_proj_path"] ~= nil then
-        default_path = vim.g["dotnet_last_proj_path"]
-      end
-
-      local path = vim.fn.input("Path to your *proj file", default_path, "file")
-
-      vim.g["dotnet_last_proj_path"] = path
-
-      local cmd = "dotnet build -c Debug " .. path .. " > /dev/null"
-
-      print("")
-      print("Cmd to execute: " .. cmd)
-
-      local f = os.execute(cmd)
-
-      if f == 0 then
-        print("\nBuild: ✔️ ")
-      else
-        print("\nBuild: ❌ (code: " .. f .. ")")
-      end
-    end
-
-    local dotnet_get_dll_path = function()
-      local request = function()
-        return vim.fn.input("Path to dll to debug: ", vim.fn.getcwd() .. "/bin/Debug/", "file")
-      end
-
-      if vim.g["dotnet_last_dll_path"] == nil then
-        vim.g["dotnet_last_dll_path"] = request()
-      else
-        if vim.fn.confirm("Change the path to dll?\n" .. vim.g["dotnet_last_dll_path"], "&yes\n&no", 2) == 1 then
-          vim.g["dotnet_last_dll_path"] = request()
-        end
-      end
-
-      return vim.g["dotnet_last_dll_path"]
-    end
-
-    dap.configurations.cs = {
-      {
-        type = "coreclr",
-        name = "Launch - coreclr (nvim-dap)",
-        request = "launch",
-        program = function()
-          if vim.fn.confirm("Rebuild first?", "&yes\n&no", 2) == 1 then
-            dotnet_build_project()
-          end
-
-          return dotnet_get_dll_path()
-        end,
-      },
-    }
-
-    local convertArgStringToArray = function(config)
-      local c = {}
-
-      for k, v in pairs(vim.deepcopy(config)) do
-        if k == "args" and type(v) == "string" then
-          c[k] = require("dap.utils").splitstr(v)
-        else
-          c[k] = v
-        end
-      end
-
-      return c
-    end
-
-    for key, _ in pairs(dap.configurations) do
-      dap.listeners.on_config[key] = convertArgStringToArray
-    end
-
+    -- Open and close dap-ui automatically
     dap.listeners.before.attach.dapui_config = function()
       dapui.open()
     end
@@ -275,6 +108,7 @@ return {
       dapui.close()
     end
 
+    -- Keybindings for DAP
     vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, { desc = "Debbuger: Toggle breakpoint" })
     vim.keymap.set("n", "<Leader>dB", function()
       local condition = vim.fn.input("Breakpoint condition (optional): ")
@@ -290,5 +124,8 @@ return {
     vim.keymap.set("n", "<Leader>dbl", dap.list_breakpoints, { desc = "Debbuger: Clear all breakpoints" })
     vim.keymap.set("n", "<Leader>do", dapui.toggle, { desc = "Debbuger: Toggle DAP UI" })
     vim.keymap.set("n", "<Leader>dc", dap.continue, { desc = "Debbuger: Continue/Launch" })
+
+    -- Language-specific debugging configuration
+    require("scripts.debugging.js").setup(dap)
   end,
 }

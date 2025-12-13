a3d4b30ff67e2cd9c635037b9525f1520cc0f980
Aliosh Romano Pimenta <alioshr@protonmail.com>
2025-12-14 00:54:02 +0100

    chore: update deps

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,21 +1,5 @@
 return {
   {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {
-      style = "night",
-    },
-    config = function()
-      -- vim.cmd.colorscheme("tokyonight")
-      -- Override unused variable colors
-      vim.api.nvim_set_hl(0, "DiagnosticUnnecessary", { fg = "#8a92b3" })
-      vim.api.nvim_set_hl(0, "@variable.unused", { fg = "#8a92b3" })
-      vim.api.nvim_set_hl(0, "@parameter.unused", { fg = "#8a92b3" })
-      -- vim.cmd.colorscheme("tokyonight")
-    end,
-  },
-  {
     "rebelot/kanagawa.nvim",
     lazy = false,
     priority = 1000,

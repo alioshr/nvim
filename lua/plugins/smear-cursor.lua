ee73c893a0f57278dd6d4429fca1754db7570dde
Aliosh Romano Pimenta <alioshr@protonmail.com>
2026-03-11 00:46:50 +0100

    fix(smear-cursor): revert to default animation values

--- lua/plugins/smear-cursor.lua
+++ lua/plugins/smear-cursor.lua
@@ -1,9 +1,9 @@
 return {
   "sphamba/smear-cursor.nvim",
   opts = {
-    stiffness = 0.8,
-    trailing_stiffness = 0.5,
-    distance_stop_animating = 0.5,
+    stiffness = 0.6,
+    trailing_stiffness = 0.45,
+    distance_stop_animating = 0.1,
     hide_target_hack = false,
     cursor_color = "#bd9dd4",
   },
